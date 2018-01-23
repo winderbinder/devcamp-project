f4fe43d2127491eaefd422d571887728c4fd227e
winderbinder <junior_19000@hotmail.com>
2018-01-23 14:16:24 -0700

    implemented show and delete functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -33,5 +33,20 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+  def destroy
+    #perfomr the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    #destroy/delete the record
+    @portfolio_item.destroy
+
+    #redirect
+    respond_to do |format|
+      format.html { redirect_to blogs_url, notice: 'Record was successfully removed.' }
+    end
+  end
 
 end
--- app/views/pages/ruby_experiment.rb
+++
@@ -1,5 +0,0 @@
-def add(add1, add2, add3)
-  add1 + add2 + add3
-
-add_numbers = [1, 5, 3]
-puts add(*add_numbers)  
