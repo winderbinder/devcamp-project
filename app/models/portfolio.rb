727a3c224bfc483074c49d0f6e49123f90e0652b
winderbinder <junior_19000@hotmail.com>
2018-01-25 14:40:06 -0700

    integrated placeholders and image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,14 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails')}
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= Placeholder.image_generator(height: '600',width: '300')
+    self.thumb_image ||= Placeholder.image_generator(height: '600',width: '300')
+  end
 end
+
+
+
