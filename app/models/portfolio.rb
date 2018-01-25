8963b89a5c397374f465147066f78ef9b7ce2207
winderbinder <junior_19000@hotmail.com>
2018-01-25 16:18:46 -0700

    configuring nested attributes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,7 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
+  accepts_nested_attributes_for :technologies
+
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
@@ -13,7 +15,7 @@ class Portfolio < ApplicationRecord
 
   def set_defaults
     self.main_image ||= Placeholder.image_generator(height: '600',width: '300')
-    self.thumb_image ||= Placeholder.image_generator(height: '600',width: '300')
+    self.thumb_image ||= Placeholder.image_generator(height: '500',width: '200')
   end
 end
 
