e96847e3c2afbf4f786885b82f01d6b2afa7442e
winderbinder <junior_19000@hotmail.com>
2018-01-29 14:06:58 -0700

    working with params

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -11,7 +11,7 @@ class PortfoliosController < ApplicationController
   end
 
     def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -30,7 +30,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Blog was successfully updated.' }
       else
         format.html { render :edit }
@@ -53,4 +53,13 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  private
+
+    def portfolio_params
+      params.require(:portfolio).permit(:title,
+                                        :subtitle, 
+                                        :body, 
+                                        technologies_attributes: [:name]
+                                        )
+  end
 end
