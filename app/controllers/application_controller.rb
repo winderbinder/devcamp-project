49b9bd3c8987068afb1738c3d19991a2ec1414e7
winderbinder <junior_19000@hotmail.com>
2018-01-29 14:59:28 -0700

    implemented guest user functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     include DeviseWhitelist
     include SetSource
+    include CurrentUserConcern
+
+    def current_user
+     super || OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@erg.com")
+  end
 end
