28a0ca053485da06158ea56625a4c3975b411a24
winderbinder <junior_19000@hotmail.com>
2018-02-06 09:51:51 -0700

    cache

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,8 @@
 class PortfoliosController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout "portfolios"
-  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :edit, :update, :sort]}, site_admin: :all
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
 
   def index
     @portfolio_items = Portfolio.by_position
