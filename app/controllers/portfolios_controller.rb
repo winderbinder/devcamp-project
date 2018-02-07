c3ac04547a23fe2f3adc5b45e48b44bddcdb4c5b
winderbinder <junior_19000@hotmail.com>
2018-02-07 11:32:52 -0700

    implemented file uploading system carrierwave for our portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -69,7 +69,9 @@ class PortfoliosController < ApplicationController
     def portfolio_params
       params.require(:portfolio).permit(:title,
                                         :subtitle, 
-                                        :body, 
+                                        :body,
+                                        :thumb_image,
+                                        :main_image, 
                                         technologies_attributes: [:name]                     
                                         )
   end
