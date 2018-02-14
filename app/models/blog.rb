ef321f11a9072ab0977b227149953f62d00ae5c4
winderbinder <junior_19000@hotmail.com>
2018-02-14 11:42:55 -0700

    updated recent scope for blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -9,4 +9,16 @@ class Blog < ApplicationRecord
 
   has_many :comments, dependent: :destroy
 
+  def self.special_blogs
+    all
+  end
+
+  def self.featured_blogs
+    limit(2)
+  end
+
+  def self.recent
+    order("created_at DESC")
+  end
+
 end
