0e109b96f6103d9c2ecb2426a64400045452666d
winderbinder <junior_19000@hotmail.com>
2018-02-16 15:51:43 -0700

    javascipt functionality fix for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://alonso-site.herokuapp.com', 'http://alonso-site.herokuapp.com']
+  config.action_cable.url = "wss://alonso-site.herokuapp.com/cable"
 end
