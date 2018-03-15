c2c1ed57a63d3bed5c816f32f807d520d4cabfd4
winderbinder <junior_19000@hotmail.com>
2018-03-15 11:37:04 -0600

    made a javascript ping

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,3 +22,8 @@
 //= require_tree .
 //= require bootstrap-sprockets
 
+var http = require("http");
+setInterval(function() {
+    http.get("https://alonso-site.herokuapp.com/");
+}, 300000); // every 5 minutes (300000)
+
--- app/assets/javascripts/ping.js
+++
@@ -1,4 +0,0 @@
-var http = require("http");
-setInterval(function() {
-    http.get("https://alonso-site.herokuapp.com/");
-}, 300000); // every 5 minutes (300000)
